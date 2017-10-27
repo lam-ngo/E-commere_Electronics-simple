122e18dd4edb6acfd0ac29fa8f3b0b287b12a911
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-27 19:23:03 +0300

    First commit

---
+++ src/main/java/ecommerce/electronics/ElectronicsApplication.java
@@ -0,0 +1,12 @@
+package ecommerce.electronics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsApplication.class, args);
+	}
+}
