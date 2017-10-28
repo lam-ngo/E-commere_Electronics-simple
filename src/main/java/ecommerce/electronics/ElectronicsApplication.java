b379eedd22ef0b65b3c56b0bbf09e7da9a099e83
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-28 16:37:34 +0300

    Modified main class to take product images from local static folder. Modified homepage to display products list

--- src/main/java/ecommerce/electronics/ElectronicsApplication.java
+++ src/main/java/ecommerce/electronics/ElectronicsApplication.java
@@ -18,12 +18,12 @@ public class ElectronicsApplication {
 	@Bean
 	public CommandLineRunner demo(ProductRepository productRepository) {
 		return (args) -> {
-			productRepository.save(new Product("Apple Iphone 8", "http://drop.ndtv.com/TECH/product_database/images/913201720152AM_635_iphone_x.jpeg", "Black", 1199.95));
-			productRepository.save(new Product("Samsung Galaxy S8", "https://cf1.s3.souqcdn.com/item/2017/03/29/22/34/46/67/item_XL_22344667_30264426.jpg", "Midnight Black", 699.95));
-			productRepository.save(new Product("Apple Iphone 7 Plus", "https://www.kickmobiles.com/content/images/thumbs/0007094_apple-iphone-7-plus-32gb-gold.jpeg", "Rose Gold", 799.95));
-			productRepository.save(new Product("Huawei Mate 9 Pro", "http://img.c.huaweistatic.com/content/dam/huawei-cbg-site/en/mkt/pdp/phones/mate9pro/img/home/pic_connectivity_01.png", "Silver", 699.95));
-			productRepository.save(new Product("Oneplus 5", "https://image4.geekbuying.com/ggo_pic/2017-06-21/2017062101437331bka1gmu.jpg", "Metal Black", 559.95));
-			productRepository.save(new Product("Google Pixel XL", "https://www.techspot.com/images/products/smartphones/org/2016-10-18-product-3.jpg", "Black", 599.95));
+			productRepository.save(new Product("Apple Iphone 8", "visual/iphonex.jpg", "Black", 1199.95));
+			productRepository.save(new Product("Samsung Galaxy S8", "visual/galaxys8.jpg", "Midnight Black", 699.95));
+			productRepository.save(new Product("Apple Iphone 7 Plus", "visual/iphone7plus.jpg", "Rose Gold", 799.95));
+			productRepository.save(new Product("Huawei Mate 9 Pro", "visual/huaweimate9pro.jpg", "Silver", 699.95));
+			productRepository.save(new Product("Oneplus 5", "visual/oneplus5.jpg", "Metal Black", 559.95));
+			productRepository.save(new Product("Google Pixel XL", "visual/googlepixelxl.jpg", "Black", 599.95));
 		};
 	}
 }
