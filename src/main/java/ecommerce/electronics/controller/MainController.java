f5fd23fc79df528bef9101eeda5ace69d1527285
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-28 14:19:34 +0300

    Added productRepository to MainController at / end points

--- src/main/java/ecommerce/electronics/controller/MainController.java
+++ src/main/java/ecommerce/electronics/controller/MainController.java
@@ -2,14 +2,22 @@ package ecommerce.electronics.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ecommerce.electronics.repository.ProductRepository;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@RequestMapping(value="/")
-	public String Homepage () {
+	public String Homepage (Model model) {
+		model.addAttribute("products", productRepository.findAll());
 		return "index";
 	}
 }
