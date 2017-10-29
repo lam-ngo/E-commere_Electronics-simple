4a454561eb4be653cd0d67cde9e74f2fb7cb0296
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-29 20:16:03 +0200

    Added ShoppingCart class to create shopping cart

---
+++ src/main/java/ecommerce/electronics/model/ShoppingCart.java
@@ -0,0 +1,65 @@
+package ecommerce.electronics.model;
+
+public class ShoppingCart {
+	
+	private Long productId;
+	private String name;
+	private String color;
+	private int totalQuantity;
+	private Double totalPrice;
+	
+	public ShoppingCart(Long productId, String name, String color, int totalQuantity, Double totalPrice) {
+		super();
+		this.productId = productId;
+		this.name = name;
+		this.color = color;
+		this.totalQuantity = totalQuantity;
+		this.totalPrice = totalPrice;
+	}
+
+	public Long getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getTotalQuantity() {
+		return totalQuantity;
+	}
+
+	public void setTotalQuantity(int totalQuantity) {
+		this.totalQuantity = totalQuantity;
+	}
+
+	public Double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(Double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@Override
+	public String toString() {
+		return "ShoppingCart [productId=" + productId + ", name=" + name + ", color=" + color + ", totalQuantity="
+				+ totalQuantity + ", totalPrice=" + totalPrice + "]";
+	}
+}
