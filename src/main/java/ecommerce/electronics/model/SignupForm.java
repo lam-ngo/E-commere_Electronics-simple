e15835421453ee802ae26ac9cd775c04c58d18fa
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-30 10:58:06 +0200

    Added SignupForm class to create signup form

---
+++ src/main/java/ecommerce/electronics/model/SignupForm.java
@@ -0,0 +1,54 @@
+package ecommerce.electronics.model;
+
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class SignupForm {
+	@NotEmpty
+    @Size(min=5, max=30)
+    private String userName = "";
+
+    @NotEmpty
+    @Size(min=7, max=30)
+    private String password = "";
+
+    @NotEmpty
+    @Size(min=7, max=30)
+    private String passwordCheck = "";
+
+    @NotEmpty
+    private String role = "USER";
+
+	public String getUsername() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPasswordCheck() {
+		return passwordCheck;
+	}
+
+	public void setPasswordCheck(String passwordCheck) {
+		this.passwordCheck = passwordCheck;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+}
