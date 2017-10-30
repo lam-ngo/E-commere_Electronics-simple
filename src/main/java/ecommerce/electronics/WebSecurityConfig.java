961a195b69a57d291b0809f61c0aa8e349eb73c9
Lam Ngo <lam.ngo.qu@gmail.com>
2017-10-30 12:15:10 +0200

    Modified WebSecurityConfig to redirect anonymous user from /cart to /login page

--- src/main/java/ecommerce/electronics/WebSecurityConfig.java
+++ src/main/java/ecommerce/electronics/WebSecurityConfig.java
@@ -1,18 +1,30 @@
 package ecommerce.electronics;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-				.anyRequest().permitAll();
+			.authorizeRequests().antMatchers("/cart").authenticated()
+			.and()
+			.authorizeRequests().anyRequest().permitAll()
+			.and()
+		.formLogin()
+			.loginPage("/login")
+	        .defaultSuccessUrl("/")
+	        .permitAll()
+	        .and()
+	    .logout()
+	        .permitAll();
 	}
 	
 	
